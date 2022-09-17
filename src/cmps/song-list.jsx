7bdef8b1413d86853d6ee7d14c0a213fd8bd5fb1
Beker <orbeker7@gmail.com>
2022-09-17 17:51:15 +0300

    worked on the menu on the song list

--- src/cmps/song-list.jsx
+++ src/cmps/song-list.jsx
@@ -7,17 +7,22 @@ export const SongList = ({ station, playCurrUrl }) => {
     // const [addToPlaylistModal, setAddToPlaylistModal] = useState(null)
     const [wantedSong, setWantedSong] = useState(null)
     const [openModal, setOpenModal] = useState(null)
-    // const [currSong, setCurrSong] = useState(null)
+    const [modalPos, setModalPos] = useState(null)
 
     const dispatch = useDispatch()
     let stations = useSelector(state => state.stationModule.stations)
-    let currPlayingSongUrl = useSelector(state => state.stationModule.currentlyPlayingUrl)
+    // let currPlayingSongUrl = useSelector(state => state.stationModule.currentlyPlayingUrl)
     stations = stations.filter(station => station.isMyStation === true)
     useEffect(() => {
         dispatch(loadStations())
     }, [])
 
-    const addToPlaylist = (song) => {
+    const addToPlaylist = (ev, song) => {
+        console.log('ev :', ev)
+        let posX = ev.pageX - ev.view.innerWidth
+        let posY = ev.pageY - 100
+        let mousePos = { posX, posY }
+        setModalPos(mousePos)
         setWantedSong(song)
         setOpenModal(true)
     }
@@ -30,14 +35,13 @@ export const SongList = ({ station, playCurrUrl }) => {
 
     const onAddToMyPlaylist = (myPlaylistIdx) => {
         setOpenModal(false)
-        console.log('stations[myPlaylistIdx] :', stations[myPlaylistIdx]._id)
         dispatch(addSongToMyPlaylist(wantedSong, stations[myPlaylistIdx]._id))
     }
 
     {
         return <div>
             {
-                openModal && <ul className="add-to-playlist-modal">
+                openModal && <ul style={{ transform: `translate(${modalPos.posX}px, ${modalPos.posY}px)` }} className="add-to-playlist-modal">
                     {stations.map((station, myPlaylistIdx) =>
                         <div key={myPlaylistIdx}>
                             <li onClick={(ev) => onAddToMyPlaylist(myPlaylistIdx)}>{station.name}</li>
@@ -48,9 +52,9 @@ export const SongList = ({ station, playCurrUrl }) => {
                 return <ol onClick={() => playCurrUrl(songIdx)} key={song.id} className="main-song-list">
                     <div>
                         <div>
-                            {songIdx}<img className="song-img" src={`${song.imgUrl}`} />
+                            {songIdx + 1}<img className="song-img" src={`${song.imgUrl}`} />
                         </div>
-                        <div className="kaki-bepita">
+                        <div className="song-list-title-container">
                             <h6>{song.title}</h6>
                             <span>Artists</span>
                         </div>
@@ -60,7 +64,7 @@ export const SongList = ({ station, playCurrUrl }) => {
                         <span>Date Added</span>
                         <button className="add-to-liked-btn" onClick={() => addToLikedPlaylist(song)}>Like</button>
                         <span className="song-duration-container">{song.songDuration}</span>
-                        <button className="add-to-playlist-btn" onClick={() => addToPlaylist(song)}>Add</button>
+                        <button className="add-to-playlist-btn" onClick={(ev) => addToPlaylist(ev, song)}>Add</button>
                     </div>
                 </ol>
             })}
