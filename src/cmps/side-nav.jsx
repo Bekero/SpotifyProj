1c9f8df069452a913da6199a201a9e46ebf4cd52
ReutMaharat <reutmaharat@gmail.com>
2022-09-19 00:23:13 +0300

    nav-bar & appheader

--- src/cmps/side-nav.jsx
+++ src/cmps/side-nav.jsx
@@ -2,21 +2,27 @@
 import { NavLink } from "react-router-dom";
 import React from 'react'
 import { CreateStation } from "../cmps/create-station";
+import HomeIcon from '../cmps/svg/home-svg'
+import SearchIcon from '../cmps/svg/search-svg'
+import LibraryIcon from '../cmps/svg/library-svg'
 
 export function SideNav() {
 
         return (
             <div className="side-nav">
 
-                <NavLink className="header-logo" to='/' > <h2> Shlomify 👧🏽 </h2></NavLink>
-                <nav className="nav-container">
-                    <NavLink to='/' >Home</NavLink>
-                    <NavLink to='/search' >Search</NavLink>
-                    <NavLink to='/collection/playlist' >Library</NavLink>
+                <NavLink className="header-logo" to='/' > Shlomify  </NavLink>
+                <nav className="nav-container flex column">
+                    <ul className="flex">
+                    <li><NavLink to='/'  className='flex'> <HomeIcon/>Home</NavLink></li>
+                   <li > <NavLink to='/search' className='flex' ><SearchIcon/>Search</NavLink> </li>
+                    <li ><NavLink to='/collection/playlist'className='flex'> <LibraryIcon/>Youre Library</NavLink></li>
+                    </ul>
                 </nav>
                 <div className="playlist-container">
                     <CreateStation />
-                    <NavLink className="like-nav-link" to='/collection/track' >Liked Songs</NavLink>
+                    <NavLink className="like-nav-link" to='/collection/track'>Liked Songs</NavLink>
+                    <div className="rr"></div>
                     {/* <MyStations /> */}
                 </div>
             </div>
---
+++ src/cmps/svg/next-page.jsx
@@ -0,0 +1,4 @@
+export default function nextPageSvg() {
+    return <svg role="img" height="24" fill="grey" width="24" viewBox="0 0 24 24"><path d="M8.043 2.793a1 1 0 000 1.414L15.836 12l-7.793 7.793a1 1 0 101.414 1.414L18.664 12 9.457 2.793a1 1 0 00-1.414 0z"></path></svg>
+
+}
