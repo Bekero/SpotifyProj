07b70e38747c96f964905b5979e8a91cd3a412f0
ReutMaharat <reutmaharat@gmail.com>
2022-09-21 20:34:33 +0300

    pulling

--- src/services/youtube.service.js
+++ src/services/youtube.service.js
@@ -5,7 +5,7 @@ export const youtubeService ={
 }
 
 function getSongs(term){
-    const API_KEY = 'AIzaSyBL-4tgjB8MxfYouEBcUPllZk2u8noV9kM'
+    const API_KEY = 'AIzaSyBL-AIzaSyBKmZyRd0g8AEKqh9tNR3VNFn4ERzmmoIY'
     const results = axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${API_KEY}&q=${term}/category=Music&maxResults=10`)
 
     return Promise.resolve(results)
