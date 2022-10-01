c9eb649fc12d6c171713fa9dc0a093df001c6a3e
Yarin <yarindayan11@gmail.com>
2022-10-01 03:37:14 +0300

    can now like song from search

--- src/cmps/search-list.jsx
+++ src/cmps/search-list.jsx
@@ -1,13 +1,14 @@
 import { SearchPreview } from "./search-preview";
 
 
-export function SearchList({ addSongToPlaylist, playCurrUrl, songDuration, addToLikedPlaylist, songDetails }) {
+export function SearchList({ addSongToPlaylist, playCurrUrl, songDuration, addToLikedPlaylist, songDetails, user }) {
     if (!songDetails || !songDetails.length) return
     return (
 
         <div className="search-songs-list-container">
             {songDetails.map((song, idx) => {
                 return <SearchPreview
+                    user={user}
                     song={song}
                     songDetails={songDetails[idx]}
                     songDuration={songDuration[idx]}
