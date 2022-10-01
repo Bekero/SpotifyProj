e9be9ea665c85bbb50813e25660f5c90dea8e24c
Beker <orbeker7@gmail.com>
2022-10-01 20:09:23 +0300

    added

--- src/cmps/side-nav.jsx
+++ src/cmps/side-nav.jsx
@@ -7,13 +7,9 @@ import SearchIcon from '../cmps/svg/search-svg'
 import LibraryIcon from '../cmps/svg/library-svg'
 import LikeLinkSvg from '../cmps/svg/like-nav-link'
 import { useState } from "react";
-<<<<<<< HEAD
-import harmonyLogo from "../assets/img/harmony-logo3.jpg"
+import harmonyLogo from "../assets/img/new-logo-h.png"
 import { useDispatch, useSelector } from "react-redux";
 import { loadStations } from "../store/station.actions";
-=======
-import harmonyLogo from "../assets/img/new-logo-h.ico"
->>>>>>> 40d1c98bebfdd36cfca0f5332763769bdf80cb35
 
 export function SideNav() {
     const [isHomeActive, setIsHomeActive] = useState(false)
