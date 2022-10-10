c22d0f620c7fd7ace0c6deacf8b2132994953e00
Yarin <yarindayan11@gmail.com>
2022-10-10 13:58:32 +0300

    login section

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,5 +1,5 @@
 import { storageService } from "./async-storage.service";
-// import { httpService } from './http.service'
+import { httpService } from './http.service'
 import { store } from "../store/store";
 import {
   socketService,
@@ -28,13 +28,13 @@ export const userService = {
 window.userService = userService;
 
 function getUsers() {
-  return storageService.query("user");
-  // return httpService.get(`user`)
+  // return storageService.query("user");
+  return httpService.get("user")
 }
 
 async function getById(userId) {
-  const user = await storageService.get("user", userId);
-  // const user = await httpService.get(`user/${userId}`)
+  // const user = await storageService.get("user", userId);
+  const user = await httpService.get(`user/${userId}`)
 
   socketService.emit(SOCKET_EMIT_USER_WATCH, userId);
   // socketService.off(SOCKET_EVENT_USER_UPDATED, onUserUpdate)
@@ -44,39 +44,42 @@ async function getById(userId) {
 }
 
 function remove(userId) {
-  return storageService.remove("user", userId);
-  // return httpService.delete(`user/${userId}`)
+  // return storageService.remove("user", userId);
+  return httpService.delete(`user/${userId}`)
 }
 
 async function update(user) {
-  await storageService.put("user", user);
-  // user = await httpService.put(`user/${user._id}`, user)
+  // await storageService.put("user", user);
+  user = await httpService.put(`user/${user._id}`, user)
   // Handle case in which admin updates other user's details
   if (getLoggedinUser()._id === user._id) saveLocalUser(user);
   return user;
 }
 
 async function login(userCred) {
-  const users = await storageService.query("user");
-  const user = users.find((user) => user.username === userCred.username);
-  // const user = await httpService.post('auth/login', userCred)
+  console.log('userCred', userCred)
+  // const users = await httpService.get("user")
+  // let user = users.find((user) => user.username === userCred.username)
+  // console.log('usersusersusersusersusers', user)
+  const user = await httpService.post('auth/login', userCred)
+  console.log('user', user);
   if (user) {
     // socketService.login(user._id)
     return saveLocalUser(user);
   }
-  return Promise.reject("abcdef u and your mom");
+  return Promise.reject("abcdef u and your mom")
 }
 
 async function signup(userCred) {
-  const users = await storageService.query("user");
+  const users = await httpService.get("user");
   const isUser = users.find((user) => user.username === userCred.username);
   if (isUser) {
     return Promise.reject("username is already taken");
   }
   userCred.likedSongs = []
-  const user = await storageService.post("user", userCred);
+  // const user = await storageService.post("user", userCred);
 
-  // const user = await httpService.post('auth/signup', userCred)
+  const user = await httpService.post('auth/signup', userCred)
   socketService.login(user._id);
   return saveLocalUser(user);
 }
@@ -84,7 +87,7 @@ async function signup(userCred) {
 async function logout() {
   sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN_USER);
   socketService.logout();
-  // return await httpService.post('auth/logout')
+  return await httpService.post('auth/logout')
 }
 
 function saveLocalUser(user) {
